dfd771915b041addf7892fe35f410f29c9605c5c
ardhi <code@ardhitama.com>
2020-04-11 19:28:11 +0700

    Add specs

--- lib/errx.ex
+++ lib/errx.ex
@@ -1,11 +1,15 @@
 defmodule Errx do
+  @type t :: %Errx{}
+
   @derive Jason.Encoder
   defstruct [:file, :func, :error, :context]
 
+  @spec wrap(any) :: {:error, Errx.t()}
   def wrap(error) do
     wrap(error, nil)
   end
 
+  @spec wrap(any, any) :: {:error, Errx.t()}
   def wrap(error, context) do
     {mod, fname, farity, [file: file, line: line]} =
       Process.info(self(), :current_stacktrace) |> elem(1) |> Enum.fetch!(2)
@@ -28,6 +32,7 @@ defmodule Errx do
     end
   end
 
+  @spec unwrap(any) :: any
   def unwrap(error) do
     case error do
       {:error, %Errx{error: %Errx{} = error}} ->
