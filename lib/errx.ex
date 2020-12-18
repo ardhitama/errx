4aa7032f4552381e5051a49147f75dd03370bec7
ardhi <ardhitama@ymail.com>
2020-12-18 18:21:31 +0700

    spec fixes

--- lib/errx.ex
+++ lib/errx.ex
@@ -3,7 +3,7 @@ defmodule Errx do
 
   defstruct [:file, :func, :reason, :parent]
 
-  @spec wrap({:error, any} | Errx.t()) :: Errx.t()
+  @spec wrap(any) :: Errx.t()
   def wrap(error) do
     {mod, fname, farity, [file: file, line: line]} =
       Process.info(self(), :current_stacktrace) |> elem(1) |> Enum.fetch!(2)
@@ -23,18 +23,12 @@ defmodule Errx do
     end
   end
 
-  @spec wrap({:error, any} | Errx.t(), {:error, any} | Errx.t()) :: Errx.t()
+  @spec wrap(any, any) :: Errx.t()
   def wrap(parent_error, child_error) do
     %Errx{wrap(child_error) | parent: wrap(parent_error)}
   end
 
   @spec match(any) :: any
-  defmacro match({:error, reason}) do
-    quote do
-      %Errx{reason: unquote(reason)}
-    end
-  end
-
   defmacro match(%Errx{reason: reason}) do
     quote do
       {:error, unquote(reason)}
@@ -47,7 +41,7 @@ defmodule Errx do
     end
   end
 
-  @spec match({:error, any} | Errx.t(), {:error, any} | Errx.t()) :: boolean
+  @spec match(any, any) :: boolean
   def match(%Errx{reason: err1}, {:error, err2}) do
     err1 == err2
   end
